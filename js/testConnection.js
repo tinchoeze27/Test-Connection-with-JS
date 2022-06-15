c3e3a0032aa351b8119620d6530b295cc24d4762
Martin <mezequiel2790@gmail.com>
2022-06-15 12:47:58 -0300

    test connection

--- js/testConnection.js
+++ js/testConnection.js
@@ -1,22 +1,22 @@
 const checkOnlineStatus = async () => {
     try {
-      const online = await fetch("/img/icon.png");
-      return online.status >= 200 && online.status < 300; // either true or false
+      const online = await fetch("/img/icon.png"); //Solicita este archivo
+      return online.status >= 200 && online.status < 300; //Si el navegador devuelve estos valores entre 200 a 299
     } catch (err) {
-      return false; // definitely offline
+      return false; 
     }
   };
   
   setInterval(async () => {
     const result = await checkOnlineStatus();
     const statusDisplay = document.getElementById("status");
-    statusDisplay.textContent = result ? "Online" : "OFFline";
-  }, 3000); // probably too often, try 30000 for every 30 seconds
+    statusDisplay.textContent = result ? "Online" : "Offline";
+  }, 3000); // Tiempo de intervalo a verificar en segundos
   
   // forgot to include async load event listener in the video! 
   window.addEventListener("load", async (event) => {
     const statusDisplay = document.getElementById("status");
     statusDisplay.textContent = (await checkOnlineStatus())
       ? "Online"
-      : "OFFline";
+      : "Offline";
   });
